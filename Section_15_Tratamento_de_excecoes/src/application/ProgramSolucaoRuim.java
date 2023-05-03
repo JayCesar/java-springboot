46e9b0a0c34083bc041384198d94612eabfc400d
salveodojuliao <batistajulio215@gmail.com>
2023-05-02 21:50:55 -0300

    add: add bad solution

--- Section_15_Tratamento_de_excecoes/src/application/ProgramSolucaoRuim.java
+++ Section_15_Tratamento_de_excecoes/src/application/ProgramSolucaoRuim.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 import model.entities.Reservation;
 
-public class SolucaoMuitoRuim {
+public class ProgramSolucaoRuim {
 
 	public static void main(String[] args) throws ParseException {
 
@@ -35,15 +35,11 @@ public class SolucaoMuitoRuim {
 			System.out.print("Check-out date (dd/MM/yyyy): ");
 			checkOut = sdf.parse(sc.next());
 
-			Date now = new Date();
-			if (checkIn.before(now) || checkOut.before(now)) {
-				System.out.println("Error in reservation: Reservation dates for update must be future dates");
-			}
-			else if (!checkOut.after(checkIn)) {
-				System.out.println("Error in reservation: Check-out date must be after check-in date");
+			String error = reservation.updateDates(checkIn, checkOut);
+			if (error != null) {
+				System.out.println("Error in reservation: " + error);
 			}
 			else {
-				reservation.updateDates(checkIn, checkOut);
 				System.out.println("Reservation: " + reservation);
 			}
 		}
--- Section_15_Tratamento_de_excecoes/src/model/entities/Reservation.java
+++ Section_15_Tratamento_de_excecoes/src/model/entities/Reservation.java
@@ -5,13 +5,13 @@ import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class Reservation {
-	
+
 	private Integer roomNumber;
 	private Date checkIn;
 	private Date checkOut;
-	
+
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-	
+
 	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
 		this.roomNumber = roomNumber;
 		this.checkIn = checkIn;
@@ -36,27 +36,33 @@ public class Reservation {
 
 	// TimeUnit é um tiipo Enumerado com operações
 	public long duration() {
-		long diff = checkOut.getTime() - checkIn.getTime(); // Calcula a diferença em milisegundos
-		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS); // Converte os milisegundos para dias
+		long diff = checkOut.getTime() - checkIn.getTime();
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);// Converte os milisegundos para dias
 	}
-	
-	public void updateDates(Date checkIn, Date checkOut) {
+
+	public String updateDates(Date checkIn, Date checkOut) {
+		Date now = new Date();
+		if (checkIn.before(now) || checkOut.before(now)) {
+			return "Reservation dates for update must be future dates";
+		}
+		if (!checkOut.after(checkIn)) {
+			return "Check-out date must be after check-in date";
+		}
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
+		return null; // Eu utilizo para dizer que não teve nenhum erro
 	}
-	
+
 	@Override
 	public String toString() {
 		return "Room "
-				+ roomNumber
-				+ ", check-in: "
-				+ sdf.format(checkIn)
-				+ ", check-out: "
-				+ sdf.format(checkOut)
-				+ ", "
-				+ duration()
-				+ " nights";	
+			+ roomNumber
+			+ ", check-in: "
+			+ sdf.format(checkIn)
+			+ ", check-out: "
+			+ sdf.format(checkOut)
+			+ ", "
+			+ duration()
+			+ " nights";
 	}
-	
-	
 }
