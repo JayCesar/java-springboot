6353b7a4a958dc893f6c153a56cd0b684337b20d
salveodojuliao <batistajulio215@gmail.com>
2023-05-04 14:52:30 -0300

    add: creates new folder with my resolution to the exercise

---
+++ Section_15_Tratamento_de_execcoes_exercise/src/app/ProgramAccount.java
@@ -0,0 +1,42 @@
+package app;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Account;
+import model.exceptions.DomainException;
+
+public class ProgramAccount {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		try {
+			System.out.println("Enter account data ");
+			System.out.print("Number: ");
+			int number = sc.nextInt();
+			String name = sc.nextLine();
+			System.out.print("Holder: ");
+			String holder = sc.nextLine();
+			System.out.print("Initial balance: ");
+			double initialBalance = sc.nextDouble();
+			System.out.print("Withdraw limit:: ");
+			double withdrawLimit = sc.nextDouble();
+			Account account = new Account(number, holder, initialBalance, withdrawLimit);
+			
+			System.out.println();
+			System.out.print("Enter amount for withdraw: ");
+			double amountWith = sc.nextDouble();
+			account.withdraw(amountWith);
+			System.out.println(account);
+		}
+		catch(DomainException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		sc.close();
+
+	}
+
+}
