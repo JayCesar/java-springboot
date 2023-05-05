84d45ca4060b2b78c7ff2298af053a6b1d31c587
salveodojuliao <batistajulio215@gmail.com>
2023-05-05 06:38:30 -0300

    add: Places pieces on the board

--- project-chess-system-java/src/boardgame/Board.java
+++ project-chess-system-java/src/boardgame/Board.java
@@ -36,6 +36,10 @@ public class Board {
 		return pieces[position.getRow()][position.getColumn()];
 	}
 	
+	public void PlacePiece(Piece piece, Position position) {
+		pieces[position.getRow()][position.getColumn()] = piece;
+		piece.position = position;
+	}
 	
 	
 }
