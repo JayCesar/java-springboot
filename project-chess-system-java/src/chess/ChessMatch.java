102f686b396e9639f6975fea7fca9d7b76f22699
salveodojuliao <batistajulio215@gmail.com>
2023-05-05 06:51:14 -0300

    add: adds BoardException and defensive programming

--- project-chess-system-java/src/chess/ChessMatch.java
+++ project-chess-system-java/src/chess/ChessMatch.java
@@ -26,7 +26,7 @@ public class ChessMatch {
 	
 	private void initialSetup() {
 		board.PlacePiece(new Rook(board, Color.WHITE), new Position(2, 1));
-		board.PlacePiece(new King(board, Color.BLACK), new Position(0, 4));
+		board.PlacePiece(new King(board, Color.BLACK), new Position(2, 4));
 		board.PlacePiece(new King(board, Color.WHITE), new Position(7, 4));
 	}
 }
