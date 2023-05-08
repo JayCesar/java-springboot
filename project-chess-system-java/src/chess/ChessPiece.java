35c2d68b5e56d59abc96e57cecec90fd178da7e8
salveodojuliao <batistajulio215@gmail.com>
2023-05-08 17:26:04 -0300

    add: add Check Logic

--- project-chess-system-java/src/chess/ChessPiece.java
+++ project-chess-system-java/src/chess/ChessPiece.java
@@ -17,10 +17,16 @@ public abstract class ChessPiece extends Piece{
 		return color;
 	}
 	
+	public ChessPosition getChessPosition() {
+		return ChessPosition.fromPosition(position);
+	}
+	
 	protected boolean isThereOpponentPiece(Position position) {
 		ChessPiece p = (ChessPiece) getBoard().piece(position);
 		return p != null && p.getColor() != color;
 	}
+	
+	
 
 	
 }
