897386c2b0456195066f401701c71f8c4b93fa8d
JayCesar <batistajulio215@gmail.com>
2023-05-18 20:19:46 -0300

    update: add Try With Resources

---
+++ TrabalhandoComArquivos/src/application/TryWithResources.java
@@ -0,0 +1,30 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TryWithResources {
+
+	public static void main(String[] args) {
+		String path = "D:\\workspace\\ws-eclipse\\java-springboot\\files_class\\file.txt";
+	
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			String line = br.readLine();
+			
+			while (line != null) {
+				System.out.println(line);
+				line = br.readLine();
+				
+			}
+		}
+		catch(IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+	
+		
+	}
+	
+
+
+}
