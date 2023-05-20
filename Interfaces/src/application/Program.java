5ceb38cf6be7e5e5049f19d344e86f1ac0ee3d27
JayCesar <batistajulio215@gmail.com>
2023-05-20 14:39:01 -0300

    update: add new java project called Interface + starting problem

---
+++ Interfaces/src/application/Program.java
@@ -0,0 +1,39 @@
+package application;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.CarRental;
+import model.entities.Vehicle;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");	
+						
+		System.out.println("Entre com os dados do aluguel");
+		System.out.print("Modelo do carro:");
+		String carModel = sc.nextLine();
+		System.out.print("Retirada (dd/MM/yyyy hh:mm): ");
+		LocalDateTime start = LocalDateTime.parse(sc.nextLine(), fmt);
+		System.out.print("Retorno (dd/MM/yyyy hh:mm): ");
+		LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), fmt);
+		
+		CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
+		
+		
+		
+		
+		
+		sc.close();
+
+	}
+
+}
