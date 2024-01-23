b80517b2a591a78f9a0858b355afb096c155abbb
JayCesar <batistajulio215@gmail.com>
2024-01-23 19:05:45 -0300

    update: add challenge + organization

--- Challenges/Faculdade/src/tela/TelaPrincipal.java
+++ Challenges/Faculdade/src/tela/TelaPrincipal.java
@@ -1,5 +1,18 @@
 package tela;
 
 public class TelaPrincipal {
+/*
+ * Código teste [comentário]
+ 
+ * */	
+	public static void main(String[] args) {
+		
+		// Apenas um comentário
+		System.out.println("Apenas um texto");
+		
+		/*
+		 Trecho de código
+		*/
+	}
 
 }
