1f868a7762f0282272ed09cae6fb775d6f3952bf
JayCesar <batistajulio215@gmail.com>
2024-01-23 18:44:12 -0300

    first files added

---
+++ Clinica/src/administraca/Agenda.java
@@ -0,0 +1,5 @@
+package administraca;
+
+public class Agenda {
+
+}
---
+++ Clinica/src/tela/TelaPrincipal.java
@@ -0,0 +1,10 @@
+package tela;
+
+public class TelaPrincipal {
+	
+	// Tudo começa no main!
+	public static void main(String[] args) {
+		
+	}
+	
+}
